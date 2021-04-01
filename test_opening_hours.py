bc246c013d815da6d007c035265b1917efb5d0c6
Ivan Nikiforov <nikiforr@gmail.com>
2021-04-01 18:41:19 +0300

    add tests for GET methods

--- test_opening_hours.py
+++ test_opening_hours.py
@@ -1,16 +1,28 @@
 import json
 import unittest
 
+import opening_hours
 from examples import example_input, example_output
-from opening_hours import app
 
 
 class TestOpeningHours(unittest.TestCase):
-    def test_shifts_handler_valid_input(self):
+    def test_shifts_handler_post_valid_input(self):
         input_data = json.loads(example_input)
-        with app.test_client() as c:
+        with opening_hours.app.test_client() as c:
             resp = c.post("/shifts", json=input_data)
             self.assertEqual(resp.data.decode(), example_output)
+
+    def test_shifts_handler_get_instructions(self):
+        expected_data = opening_hours.show_shifts_endpoint_instructions()
+        with opening_hours.app.test_client() as c:
+            resp = c.get("/shifts")
+            self.assertEqual(resp.data.decode(), expected_data)
+
+    def test_root_handler_get(self):
+        expected_data = opening_hours.show_shifts_endpoint_instructions()
+        with opening_hours.app.test_client() as c:
+            resp = c.get("/", follow_redirects=True)
+            self.assertEqual(resp.data.decode(), expected_data)
 
 
 if __name__ == '__main__':
